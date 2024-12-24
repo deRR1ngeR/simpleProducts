ee53311f6ee9bf766241c4c404f7f79dcb3543fe
Andrsko <Saykotechka76@mail.ru>
2024-12-24 15:55:27 +0300

    Изменение роута

--- frontend/src/app/products/manage/page.tsx
+++ frontend/src/app/products/manage/page.tsx
@@ -9,7 +9,7 @@ export default function ManageProductsPage() {
   const handleFormSubmit = async (formData: any) => {
     console.log(formData);
 
-    await fetch("http://localhost:3001/products", {
+    await fetch("https://simpleproducts.railway.internal/products", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,4 +1,5 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://simpleproducts.railway.internal";
 
 export const fetcher = async (endpoint: string, options = {}) => {
   const response = await fetch(`${API_URL}${endpoint}`, options);
